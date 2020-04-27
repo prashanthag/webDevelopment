86dbb0d310f8397e3bd778ded371978b9f02cefa
Prashantha Gowda <prashantha.g@gmail.com>
2020-04-26 23:25:05 -0700

    * some projects submitted

--- fullstack/app/app.py
+++ fullstack/app/app.py
@@ -1,63 +1,111 @@
-import sys 
-from flask import Flask ,render_template ,request,redirect,url_for,abort,jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
-app = Flask(__name__)
+from flask_migrate import Migrate
+import sys
 
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres@localhost:5432/app'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class TodoList(db.Model):
+  __tablename__ = 'todolists'
+  id = db.Column(db.Integer,primary_key = True)
+  name = db.Column(db.String(),nullable = False)
+  todos = db.relationship('Todo',backref='list',cascade='all, delete-orphan',lazy=True)
+  def __repr__(self):
+    return f'<TodoList {self.id} {self.name}>'
+
+order_items = db.Table('order_items',
+    db.Column('order_id', db.Integer, db.ForeignKey('order.id'), primary_key=True),
+    db.Column('product_id', db.Integer, db.ForeignKey('product.id'), primary_key=True)
+)
+
+class Order(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  status = db.Column(db.String(), nullable=False)
+  products = db.relationship('Product', secondary=order_items,
+      backref=db.backref('orders', lazy=True))
+
+class Product(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(), nullable=False)
+  
 
 class Todo(db.Model):
-    __tablename__='Todo'
-    id = db.Column(db.Integer,primary_key=True)
-    description = db.Column(db.String(),nullable=False)
+  __tablename__ = 'Todo'
+  id = db.Column(db.Integer, primary_key=True)
+  description = db.Column(db.String(), nullable=False)
+  completed = db.Column(db.Boolean, nullable=False)
+  list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=True)
+  def __repr__(self):
+    return f'<Todo {self.id} {self.description}, list {self.list_id}>'      
 
-    def __repr__(self):
-        return f'Todo {self.id}, {self.description}'
 
-#db.session.add(Data(des))
-db.create_all()
-#person = Data(description='bull')
-#db.session.add(person)
-#db.session.commit()
+@app.route('/todos/<todo_id>', methods=['DELETE'])
+def delete_todo(todo_id):
+  try:
+    Todo.query.filter_by(id=todo_id).delete()
+    db.session.commit()
+  except:
+    db.session.rollback()
+  finally:
+    db.session.close()
+  return jsonify({ 'success': True })
+
+# note: more conventionally, we would write a
+# POST endpoint to /todos for the create endpoint:
+# @app.route('/todos', method=['POST'])
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-    error = False
-    body = {}
-    try:
-        #description = request.form.get('description','')
-        description = request.get_json()['description']
-        todo = Todo(description=description)
-        db.session.add(todo)
-        db.session.commit()
-        #return redirect(url_for('index'))
-        body ['description'] = todo.description
-    except:
-        error = True
-        db.session.rollback()
-        print(sys.exc_info())
-    finally:
-        db.session.close()
-    if not error:
-        return  jsonify(body)
+  error = False
+  body = {}
+  try:
+    description = request.get_json()['description']
+    todo = Todo(description=description, completed=False)
+    db.session.add(todo)
+    db.session.commit()
+    body['id'] = todo.id
+    body['completed'] = todo.completed
+    body['description'] = todo.description
+  except:
+    error = True
+    db.session.rollback()
+    print(sys.exc_info())
+  finally:
+    db.session.close()
+  if error:
+    abort (400)
+  else:
+    return jsonify(body)
+
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+  try:
+    completed = request.get_json()['completed']
+    print('completed', completed)
+    todo = Todo.query.get(todo_id)
+    todo.completed = completed
+    db.session.commit()
+  except:
+    db.session.rollback()
+  finally:
+    db.session.close()
+  return redirect(url_for('index'))
+
+@app.route('/lists/<list_id>')
+def get_lists_todos(list_id):
+
+  return render_template('index.html', 
+  lists = TodoList.query.all(),
+  active_list=TodoList.query.get(list_id),
+  todos = Todo.query.filter_by(list_id=list_id ).order_by('id').all())
+
 @app.route('/')
 def index():
-   """
-   return render_template('index.html',data=[{
-        'description':"Todo 1"
-    },
-    {
-        'description':"Todo 2"
-    },
-    {
-        'description':"Todo 3"
-    }
-    ])
-    """
-
-
-   return render_template('index.html',data=Todo.query.all())
+  return redirect(url_for('get_lists_todos',list_id = 1))
 
 
 if __name__ == '__main__':
-  app.run()
+   app.run(debug = True)
